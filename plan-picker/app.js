ff2dd61c3733b4592a4f2da201a184df8cb85388
Daniel Kelly <me@danielkelly.io>
2021-06-17 14:37:25 -0500

    5. global vs local components

--- plan-picker/app.js
+++ plan-picker/app.js
@@ -1,16 +1,21 @@
-const app = Vue.createApp({})
-.component('plan', {
+let PlanComponent = {
   template: '#plan-template',
   props: {
     name: { type: String, required: true }
   }
-})
-.component('plan-picker', {
+}
+
+let PlanPickerComponent = {
+  components: { plan: PlanComponent },
   template: '#plan-picker-template',
   data(){
     return {
       plans: ['The Single', 'The Curious', 'The Addict']
     }
   }
+}
+
+const app = Vue.createApp({
+  components: { PlanPicker: PlanPickerComponent }
 })
 .mount('#app')
