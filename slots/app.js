1ca7b69e0c6f82b50d28362ca6a3f02d3599ea42
Daniel Kelly <me@danielkelly.io>
2021-06-17 15:07:42 -0500

    9. component slots

--- slots/app.js
+++ slots/app.js
@@ -1,2 +1,12 @@
-const app = Vue.createApp({})
+const app = Vue.createApp({
+
+})
+.component('todo-item', {
+  template: '#todo-item-template',
+  data(){
+    return {
+      done: false
+    }
+  }
+})
 .mount('#app')
