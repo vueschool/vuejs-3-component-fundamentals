aebeef4027418fc503aab506adc119b4525a71ce
Daniel Kelly <me@danielkelly.io>
2021-06-17 14:32:09 -0500

    4. nested components

--- plan-picker/app.js
+++ plan-picker/app.js
@@ -1,14 +1,16 @@
-const app = Vue.createApp({
-  data(){
-    return {
-      plans: ['The Single', 'The Curious', 'The Addict']
-    }
-  }
-})
+const app = Vue.createApp({})
 .component('plan', {
   template: '#plan-template',
   props: {
     name: { type: String, required: true }
+  }
+})
+.component('plan-picker', {
+  template: '#plan-picker-template',
+  data(){
+    return {
+      plans: ['The Single', 'The Curious', 'The Addict']
+    }
   }
 })
 .mount('#app')
