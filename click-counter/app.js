2f0f96958f68a8816114b440ed3ff6b6885c52f3
Daniel Kelly <me@danielkelly.io>
2021-06-17 14:11:51 -0500

    2. component template

--- click-counter/app.js
+++ click-counter/app.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({
 
 })
 .component('click-counter', {
-  template: '<button @click="count++">{{count}}</button>',
+  template: '#click-counter-template',
   data(){
     return {
       count: 0
