0a9e3169acd58a394def207b3d7968d59c12bcb5
Daniel Kelly <me@danielkelly.io>
2021-06-17 14:09:08 -0500

    1. introduction to components

--- click-counter/app.js
+++ click-counter/app.js
@@ -1,4 +1,12 @@
 const app = Vue.createApp({
 
 })
+.component('click-counter', {
+  template: '<button @click="count++">{{count}}</button>',
+  data(){
+    return {
+      count: 0
+    }
+  }
+})
 .mount('#app')
