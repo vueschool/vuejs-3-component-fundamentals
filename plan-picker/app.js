3f4dd4a1f2b6b1eceff1787a9ca700c0b3b8c8be
rashiop <red.alpacca@gmail.com>
2021-08-19 16:53:38 +0700

    global component

--- plan-picker/app.js
+++ plan-picker/app.js
@@ -1,4 +1,18 @@
 const app = Vue.createApp({
-  
+})
+.component('plan-picker', {
+  template: '#plan-picker-template',
+  data() {
+    return {
+      plans: ['The Hacker', 'The Addict', 'The Curious']
+    }
+  }
+})
+.component('plan', {
+  template: '#plan-template',
+  props: {
+    name: { type: String, default: 'Popsy', required: true },
+    price: Number
+  },
 })
 .mount('#app') 
