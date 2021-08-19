0bd0cd7751181103da36c86a71270bcc4a132a73
rashiop <red.alpacca@gmail.com>
2021-08-19 17:26:37 +0700

    feat: local components

--- plan-picker/app.js
+++ plan-picker/app.js
@@ -1,18 +1,35 @@
-const app = Vue.createApp({
-})
-.component('plan-picker', {
-  template: '#plan-picker-template',
-  data() {
-    return {
-      plans: ['The Hacker', 'The Addict', 'The Curious']
-    }
-  }
-})
-.component('plan', {
+let PlanComponent = {
   template: '#plan-template',
   props: {
     name: { type: String, default: 'Popsy', required: true },
-    price: Number
+    price: Number,
+    selected: { type: Boolean, default: false }
   },
+  methods: {
+    select() {
+      this.selected = true
+      this.$emit('select', this.name)
+    }
+  }
+}
+
+let PlanPickerComponent = {
+  components: { plan: PlanComponent },
+  template: '#plan-picker-template',
+  data() {
+    return {
+      plans: ['The Hacker', 'The Addict', 'The Curious'],
+      selectedPlan: null,
+    }
+  },
+  methods: {
+    selectPlan(plan) {
+      this.selectedPlan = plan
+    }
+  }
+}
+
+const app = Vue.createApp({
+  components: { 'plan-picker': PlanPickerComponent },
 })
 .mount('#app') 
