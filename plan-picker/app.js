ddb99636071ea5705760ba4c99f8d37c307f4e83
rashiop <red.alpacca@gmail.com>
2021-08-19 14:28:32 +0700

    settings: plan start

--- plan-picker/app.js
+++ plan-picker/app.js
@@ -1,33 +1,4 @@
-let PlanPickerItemComponent = {
-  template: '#plan-picker-item-template',
-  props: {
-    name: { type: String, required: true },
-    selected: { type: Boolean, default: false },
-  },
-  methods:{
-    select(){
-      this.$emit('select', this.name)
-    }
-  }
-}
-
-let PlanPickerComponent = {
-  components: { 'plan-picker-item': PlanPickerItemComponent },
-  template: '#plan-picker-template',
-  data(){
-    return {
-      plans: ['The Single', 'The Curious', 'The Addict'],
-      selectedPlan: null
-    }
-  },
-  methods:{
-    selectPlan(plan){
-      this.selectedPlan = plan
-    }
-  }
-}
-
 const app = Vue.createApp({
-  components: { PlanPicker: PlanPickerComponent }
+  
 })
-.mount('#app')
+.mount('#app') 
